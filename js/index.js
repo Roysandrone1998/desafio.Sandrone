0cf9ec5249b97607f03314f6147afa32670c7f30
Roysandrone1998 <raysandrone@gmail.com>
2023-11-01 20:29:04 -0300

    trabajando

--- js/index.js
+++ js/index.js
@@ -8,7 +8,8 @@ class ProductManager {
         getProducts() {
         return this.products;
         }
-    
+        
+        
         agregarProducto(products) {
         products.precio += products.precio * this.#precioBaseGanancia;
     
@@ -43,7 +44,7 @@ class ProductManager {
             return "El producto no existe";
         }
     
-        const newProduct = {
+        const productoNuevo = {
             ...product,
             lugar: nuevoLanzamiento,
             fecha: nuevaFecha,
@@ -51,45 +52,47 @@ class ProductManager {
             stock: [],
         };
     
-        this.products.push(newProduct);
+        this.products.push(productoNuevo);
         }
     }
     
     class Product {
         constructor(
         nombre,
-        lugar,
+        descripcion,
         precio,
+        imagen,
         capacidad = 50,
         fecha = new Date().toLocaleDateString()
         ) {
         this.nombre = nombre;
-        this.lugar = lugar;
+        this.descripcion = descripcion;
         this.precio = precio;
+        this.imagen = imagen;
         this.capacidad = capacidad;
         this.fecha = fecha;
         this.stock = [];
         }
     }
     
-    //Pruebas
-    const manejadorProducts = new ProductManager();
+    //Pruebando
+    const prueba = new ProductManager();
     
     console.log(
-        "agregando Producto coder 1 para Argentina, precio: 200, para 50 stock"
+        "agregando Producto funda 1 para Argentina, precio: 2000, 50 stock"
     );
-    manejadorProducts.agregarProduct(
-        new Product("Producto coder 1", "Argentina", 200, 50)
+    prueba.agregarProduct(
+        new Product("Producto funda 1", "Argentina", 2000, 50)
     );
     
     console.log(
         "agregando al Producto con id 1 el stock del elemento con id 2"
     );
-    manejadorProducts.agregarElemento(1, 2);
+    prueba.agregarElemento(1, 2);
     
     console.log(
-        "creando una copia vacía del producto 1 pero en mexico y para el 2024"
+        "creando una copia vacía del producto 1 para España y para el 2023"
     );
-    manejadorProducts.ponerProductoEnMuestra(1, "Mexico", "30/11/2024");
+    prueba.ponerProductoEnMuestra(1, "España", "23/12/2023");
     
-    console.log(manejadorProducts.getProducts());
+    console.log(prueba.getProducts());
