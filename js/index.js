b59a0cc4dc0e171dc33eb90eb1a03d824824ee55
Roysandrone1998 <raysandrone@gmail.com>
2023-11-01 20:31:28 -0300

    corrigiendo

--- js/index.js
+++ js/index.js
@@ -1,23 +1,18 @@
 class ProductManager {
     #precioBaseGanancia = 0.15;
-    
-        constructor() {
-        this.products = [];
-        }
+    products = [];
     
         getProducts() {
         return this.products;
         }
-        
-        
-        agregarProducto(products) {
-        products.precio += products.precio * this.#precioBaseGanancia;
+    
+        agregarProducto(product) {
+        product.precio += product.precio * this.#precioBaseGanancia;
     
         if (this.products.length === 0) {
-            products.id = 1;
+            product.id = 1;
         } else {
-            // Autoincremental
-            Product.id = this.products[this.products.length - 1].id + 1;
+            product.id = this.products[this.products.length - 1].id + 1;
         }
     
         this.products.push(product);
@@ -30,8 +25,8 @@ class ProductManager {
             return "No existe el producto";
         }
     
-        if (!product.title.includes(idElemento)) {
-            evento.title.push(idElemento);
+        if (!product.stock.includes(idElemento)) {
+            product.stock.push(idElemento);
         } else {
             return "El elemento ya existe";
         }
@@ -75,24 +70,16 @@ class ProductManager {
         }
     }
     
-    //Pruebando
+    // Pruebas
     const prueba = new ProductManager();
     
-    console.log(
-        "agregando Producto funda 1 para Argentina, precio: 2000, 50 stock"
-    );
-    prueba.agregarProduct(
-        new Product("Producto funda 1", "Argentina", 2000, 50)
-    );
+    console.log("Agregando Producto funda 1 para Argentina, precio: 2000, 50 stock");
+    prueba.agregarProducto(new Product("Producto funda 1", "Argentina", 2000, "imagen.jpg", 50));
     
-    console.log(
-        "agregando al Producto con id 1 el stock del elemento con id 2"
-    );
+    console.log("Agregando al Producto con id 1 el stock del elemento con id 2");
     prueba.agregarElemento(1, 2);
     
-    console.log(
-        "creando una copia vacía del producto 1 para España y para el 2023"
-    );
+    console.log("Creando una copia vacía del producto 1 para España y para el 2023");
     prueba.ponerProductoEnMuestra(1, "España", "23/12/2023");
     
     console.log(prueba.getProducts());
